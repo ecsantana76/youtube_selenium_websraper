31f979f8d29aac66093e8dabb13dae790d9d5d28
Everton Santana <everton.santana@gmail.com>
2021-12-26 00:42:42 +0000

    last Beautiful attempt

--- scraper.py
+++ scraper.py
@@ -13,5 +13,10 @@ else:
 with open('trending.html', 'w') as f:
   f.write(response.text)
 
+doc = BeautifulSoup(response.text, 'html.parser')
+print('Page Title: ', doc.title.text)
 
+# Find all the Video Divs
+video_divs = doc.find_all('div', class_='ytd-video-renderer')
 
+print(f'Found {len(video_divs)} Videos')
