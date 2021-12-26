cc662c6b6f0781ad508ccea1905c0652f061481c
Everton Santana <everton.santana@gmail.com>
2021-12-26 01:52:46 +0000

    able to find and print the number of Trending Videos fixing a deprecated Selenium syntax

--- scraper.py
+++ scraper.py
@@ -1,16 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+def get_driver():
+  chrome_options = Options()
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('headless')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  driver = webdriver.Chrome(options=chrome_options)
+  return driver
 
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('headless')
-chrome_options.add_argument('--disable-dev-shm-usage')
+if __name__ == "__main__":
+  print('Creating Driver')
+  driver = get_driver()
 
-driver = webdriver.Chrome(options=chrome_options)
+  print('Fetching the Page')
+  driver.get(YOUTUBE_TRENDING_URL)
 
-driver.get(YOUTUBE_TRENDING_URL)
-print('Page Title ', driver.title)
+  print('Getting the Video Divs')
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
+  get_video_divs = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
+
+  print(f'Found {len(get_video_divs)} Videos')
