97a7b0fb2a43e9132a8a82cd30124196f4e91b4e
Everton Santana <everton.santana@gmail.com>
2021-12-26 21:03:06 +0000

    1) created functions to parse info for the Trending videos 2) created a DataFrame to export to a CSV file

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
 
@@ -18,16 +19,36 @@ def get_videos(driver):
   videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
   return videos
 
-if __name__ == "__main__":
-  driver = get_driver()
-  videos = get_videos(driver)
-  
-  video = videos[0]
+def parse_videos(video):
   title_tag = video.find_element(By.ID, 'video-title')
-  
   title = title_tag.text
   url = title_tag.get_attribute('href')
 
-  print(f'Found {len(videos)} Videos')
-  print('First Video Title: ', title)
-  print('First Video URL: ', url)
+  thumbnail_tag = video.find_element(By.TAG_NAME, 'img')
+  thumbnail_url = thumbnail_tag.get_attribute('src')
+
+  channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
+  channel_name = channel_div.text
+
+  description = video.find_element(By.ID, 'description-text').text
+  return {
+    'Title': title,
+    'URL': url,
+    'Thumbnail': thumbnail_url,
+    'Channel': channel_name,
+    'Description': description
+  }
+
+if __name__ == "__main__":
+  driver = get_driver()
+  videos = get_videos(driver)
+  video = videos[0]
+  
+print(f'Found {len(videos)} videos')
+
+videos_data = [parse_videos(video) for video in videos]
+
+print('Save the data to a CSV')
+videos_df = pd.DataFrame(videos_data)
+print(videos_df)
+videos_df.to_csv('trending.csv')
