d5edcae239586b2bdd7d97322fd9a74a1391cead
Everton Santana <everton.santana@gmail.com>
2021-12-26 00:18:27 +0000

    added requests library saved HTML file for offline access imported Beautiful Soup

--- scraper.py
+++ scraper.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import requests as req
+from bs4 import BeautifulSoup
+
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+
+response = req.get(YOUTUBE_TRENDING_URL)
+
+if response.status_code == 200:
+  print('Request Successful')
+else:
+  print('Request Unsuccessful')
+
+with open('trending.html', 'w') as f:
+  f.write(response.text)
+
+
+
